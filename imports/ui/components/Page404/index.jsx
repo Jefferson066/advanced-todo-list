d50b053800b37a59e23eedaef49c133edc226abe
Jefferson Santos <jeffersonscr12@gmail.com>
2021-08-17 23:07:20 -0300

    component 404

---
+++ imports/ui/components/Page404/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const P404 = () => (
+  <div className="404">
+    <h1>Erro 404 - Página não encontrada</h1>
+  </div>
+);
--- imports/ui/pages/Page404/index.jsx
+++ imports/ui/pages/Page404/index.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
+import { P404 } from '../../components/Page404';
 
 export const Page404 = () => {
-  return (
-    <div className="404">
-      <h1>Erro 404 - Página não encontrada</h1>
-    </div>
-  );
+  return <P404 />;
 };
