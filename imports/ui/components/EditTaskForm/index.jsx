3444e97494196912f10f766dc414b1a6775be929
Jefferson Santos <jeffersonscr12@gmail.com>
2021-08-24 15:53:29 -0300

    dit tarefa 2

---
+++ imports/ui/components/EditTaskForm/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { InputName } from '../InputName';
+import { InputText } from '../InputText';
+import { InputData } from '../InputData';
+import { BtnSubmit } from '../BtnSubmit';
+import { MyTypography } from '../MyTypography';
+import { Btn } from '../Btn';
+import { InputStatus } from '../InputStatus';
+
+export const EditTaskForm = ({
+  handleSubmit,
+  handleBackClick,
+  handleStatusChange,
+  msg,
+  viewName,
+  viewText,
+  viewData,
+  viewStatus,
+  setViewName,
+  setViewText,
+  setViewData,
+}) => {
+  return (
+    <Container maxWidth="sm">
+      <form className="task-form" onSubmit={handleSubmit}>
+        <MyTypography variant={'h4'} textValue={'Adicionar Tarefa'} />
+        {msg && (
+          <div className="msg-success">
+            <MyTypography variant={'h5'} textValue={msg} />
+          </div>
+        )}
+        <div className="input">
+          <InputName name={'name'} value={viewName} label={'Nome'} setName={setViewName} />
+        </div>
+        <div className="input">
+          <InputText
+            name={'description'}
+            value={viewText}
+            label="Descrição"
+            setText={setViewText}
+          />
+        </div>
+        <div className="input">
+          <InputData value={viewData} setData={setViewData} />
+        </div>
+        <div className="input">
+          <InputStatus viewStatus={viewStatus} handleStatusChange={handleStatusChange} />
+        </div>
+        <div className="btn">
+          <BtnSubmit textValue={'Editar'} />
+        </div>
+      </form>
+      <div className="center">
+        <Btn textValue={'Voltar'} event={handleBackClick} />
+      </div>
+    </Container>
+  );
+};
--- imports/ui/pages/EditTask/index.jsx
+++ imports/ui/pages/EditTask/index.jsx
@@ -7,16 +7,8 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
 import { BtnLogout } from '../../components/BtnLogout';
-import { Btn } from '../../components/Btn';
-
-import { Container } from '@material-ui/core';
 import { NotPermissionEdit } from '../../components/NotPermissionEdit';
-import { MyTypography } from '../../components/MyTypography';
-import { InputName } from '../../components/InputName';
-import { InputText } from '../../components/InputText';
-import { InputData } from '../../components/InputData';
-import { BtnSubmit } from '../../components/BtnSubmit';
-import { InputStatus } from '../../components/InputStatus';
+import { EditTaskForm } from '../../components/EditTaskForm';
 
 const URL_PATHS = {
   TODOLIST: '/authenticated/todolist',
@@ -30,7 +22,6 @@ export const EditTask = ({ history }) => {
   const [viewText, setViewText] = useState('');
   const [viewData, setViewData] = useState('');
   const [viewStatus, setViewStatus] = useState('');
-  // eslint-disable-next-line no-unused-vars
   const [msg, setMsg] = useState('');
 
   useEffect(() => {
@@ -39,8 +30,6 @@ export const EditTask = ({ history }) => {
     setViewData(task.data);
     setViewStatus(task.status);
   }, [task.name, task.text, task.data, task.status]);
-
-  console.log(viewName, viewText, viewData, viewStatus);
 
   const handleBackClick = (e) => {
     e.preventDefault();
@@ -51,15 +40,11 @@ export const EditTask = ({ history }) => {
     e.preventDefault();
 
     if (!viewName || !viewText || !viewData) {
-      setMsg('');
       return;
     }
 
     Meteor.call('tasks.update', _id, viewName, viewText, viewData, viewStatus, user.username);
     setMsg('Tarefa Editada!');
-    //setText('');
-    //setName('');
-    // setData('');
   };
 
   const handleStatusChange = (e) => {
@@ -77,39 +62,19 @@ export const EditTask = ({ history }) => {
         </div>
       ) : (
         <div className="main">
-          <Container maxWidth="sm">
-            <form className="task-form" onSubmit={handleSubmit}>
-              <MyTypography variant={'h4'} textValue={'Adicionar Tarefa'} />
-              {msg && (
-                <div className="msg-success">
-                  <MyTypography variant={'h5'} textValue={msg} />
-                </div>
-              )}
-              <div className="input">
-                <InputName name={'name'} value={viewName} label={'Nome'} setName={setViewName} />
-              </div>
-              <div className="input">
-                <InputText
-                  name={'description'}
-                  value={viewText}
-                  label="Descrição"
-                  setText={setViewText}
-                />
-              </div>
-              <div className="input">
-                <InputData value={viewData} setData={setViewData} />
-              </div>
-              <div className="input">
-                <InputStatus viewStatus={viewStatus} handleStatusChange={handleStatusChange} />
-              </div>
-              <div className="btn">
-                <BtnSubmit textValue={'Editar'} />
-              </div>
-            </form>
-            <div className="center">
-              <Btn textValue={'Voltar'} event={handleBackClick} />
-            </div>
-          </Container>
+          <EditTaskForm
+            handleBackClick={handleBackClick}
+            handleSubmit={handleSubmit}
+            handleStatusChange={handleStatusChange}
+            msg={msg}
+            viewName={viewName}
+            viewText={viewText}
+            viewData={viewData}
+            viewStatus={viewStatus}
+            setViewName={setViewName}
+            setViewText={setViewText}
+            setViewData={setViewData}
+          />
         </div>
       )}
     </div>
