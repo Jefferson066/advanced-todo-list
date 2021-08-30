0e727305c9e4252283e39fce063d0357bd407516
Jefferson Santos <jeffersonscr12@gmail.com>
2021-08-29 21:10:51 -0300

    cards dashboard

--- imports/ui/pages/Welcome/index.jsx
+++ imports/ui/pages/Welcome/index.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import _ from 'lodash';
 import { useTracker } from 'meteor/react-meteor-data';
-import { Btn } from '../../components/Btn';
 import { MyTypography } from '../../components/MyTypography';
 import { MyDrawer } from '../../components/Drawer';
 import { TasksCollection } from '../../../api/database/TasksCollection';
+import { Container } from '@material-ui/core';
+import { SimpleCard } from '../../components/Card';
+import { CardButton } from '../../components/CardButton';
 
+/*
 const URL_PATHS = {
   TODOLIST: '/authenticated/todolist',
-};
+};*/
 
+// eslint-disable-next-line no-unused-vars
 export const Welcome = ({ history }) => {
   const user = useTracker(() => Meteor.user());
 
@@ -37,31 +41,31 @@ export const Welcome = ({ history }) => {
   const tasksProgress = allTasksFilter.filter((obj) => obj.status === 'andamento');
   const completedTasks = allTasksFilter.filter((obj) => obj.status === 'concluida');
 
+  /*
   const handleClick = (e) => {
     e.preventDefault();
     history.push(URL_PATHS.TODOLIST);
   };
+  */
 
   return (
     <div className="app">
       <div className="main">
         <MyDrawer />
-        <MyTypography
-          variant={'h4'}
-          textValue={`Olá ${user.username}, seja bem vindo ao Todo List!`}
-        />
-        <div>
-          <h2>Cdastradas{registeredTasks.length}</h2>
-        </div>
-        <div>
-          <h2>Andamento{tasksProgress.length}</h2>
-        </div>
-        <div>
-          <h2>Concluidas{completedTasks.length}</h2>
-        </div>
-        <div className=" center">
-          <Btn event={handleClick} textValue={'Visualizar Tarefas'} />
-        </div>
+        <Container maxWidth="sm">
+          <MyTypography
+            variant={'h4'}
+            textValue={`Olá ${user.username}, seja bem vindo ao Todo List!`}
+          />
+          <div className="card">
+            <SimpleCard title={'Cadastradas'} textValue={registeredTasks.length} />
+            <SimpleCard title={'Andamento'} textValue={tasksProgress.length} />
+          </div>
+          <div className="card">
+            <SimpleCard title={'Concluídas'} textValue={completedTasks.length} />
+            <CardButton title={'Visualizar Tarefas'} />
+          </div>
+        </Container>
       </div>
     </div>
   );
